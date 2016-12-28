bb7cea8a554e4e95e41eff726a50bde4fae759e9
Jacek Lawniczak <jacekelgda@gmail.com>
2016-12-28 23:18:13 +0100

    f

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
+const router = new express.Router()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.send('Hello World! I am here')
-})
+router.post('/solutions', function (req, res) {
+    console.log(req.body);
+}
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ' + app.get('port'))
