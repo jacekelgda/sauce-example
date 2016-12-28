c1ece0297291dcc2521e6939a004293e2ea5f11f
Jacek Lawniczak <jacekelgda@gmail.com>
2016-12-28 23:34:06 +0100

    f

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 router.post('/solutions', function (req, res) {
-    console.log(req);
+    console.log(req.body);
 })
 module.exports = router
 app.use('/api', router);
