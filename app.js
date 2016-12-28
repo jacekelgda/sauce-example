28c3047cd90fd47674a7a83daa5c38ee4d2e3941
Jacek Lawniczak <jacekelgda@gmail.com>
2016-12-28 23:30:03 +0100

    f

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express')
+const bodyParser = require('body-parser')
 var app = express()
 const router = new express.Router()
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 router.post('/solutions', function (req, res) {
     console.log(req.body);
 })
-
 module.exports = router
+app.use('/api', router);
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ' + app.get('port'))
