e360aa77f2790389c1b4da6fe6d8d22808002d42
Jacek Lawniczak <jacekelgda@gmail.com>
2016-12-28 23:02:51 +0100

    init

---
+++ test.js
@@ -0,0 +1,25 @@
+var webdriver = require('selenium-webdriver'),
+    username = process.env.SAUCE_USERNAME,
+    accessKey = process.env.SAUCE_ACCESS_KEY,
+    driver;
+
+driver = new webdriver.Builder().
+  withCapabilities({
+    'browserName': 'chrome',
+    'platform': 'Windows XP',
+    'version': '43.0',
+    'username': username,
+    'accessKey': accessKey
+  }).
+  usingServer("http://" + username + ":" + accessKey +
+              "@ondemand.saucelabs.com:80/wd/hub").
+  build();
+
+// driver.get('http://saucelabs.com/test/guinea-pig');
+driver.get('http://x-team.com');
+
+driver.getTitle().then(function (title) {
+    console.log("title is: " + title);
+});
+
+driver.quit();
