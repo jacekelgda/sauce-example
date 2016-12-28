bea274da8e8fe80798cbdcece6d142f41bdb13be
Jacek Lawniczak <jacekelgda@gmail.com>
2016-12-28 23:59:03 +0100

    f

--- app.js
+++ app.js
@@ -3,29 +3,27 @@ const bodyParser = require('body-parser')
 var app = express()
 const router = new express.Router()
 
-var webdriver = require('selenium-webdriver'),
-    username = process.env.SAUCE_USERNAME,
-    accessKey = process.env.SAUCE_ACCESS_KEY,
-    driver;
-
-driver = new webdriver.Builder().
-  withCapabilities({
-    'browserName': 'chrome',
-    'platform': 'Windows XP',
-    'version': '43.0',
-    'username': username,
-    'accessKey': accessKey
-  }).
-  usingServer("http://" + username + ":" + accessKey +
-              "@ondemand.saucelabs.com:80/wd/hub").
-  build();
-
-
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 router.post('/solutions', function (req, res) {
+    var webdriver = require('selenium-webdriver'),
+        username = process.env.SAUCE_USERNAME,
+        accessKey = process.env.SAUCE_ACCESS_KEY,
+        driver;
+
+    driver = new webdriver.Builder().
+      withCapabilities({
+        'browserName': 'chrome',
+        'platform': 'Windows XP',
+        'version': '43.0',
+        'username': username,
+        'accessKey': accessKey
+      }).
+      usingServer("http://" + username + ":" + accessKey +
+                  "@ondemand.saucelabs.com:80/wd/hub").
+      build();
     driver.get(req.body);
 
     driver.getTitle().then(function (title) {
